1ecbd315668eb4c68e609eeae4239e17d97d2a0a
cartoinddigo <34676078+cartoinddigo@users.noreply.github.com>
2019-03-13 15:59:16 +0100

    maj

--- velo/views.py
+++ velo/views.py
@@ -6,10 +6,7 @@ from .models import Player
 from .forms import PlayerForm
 import csv
 from django.http import HttpResponse
-#Pour export pdf
-from django.template.loader import render_to_string
-from weasyprint import HTML
-import tempfile
+
 
 
 
@@ -22,26 +19,6 @@ def detailsplayer(request, pk):
 	players = get_object_or_404(Player, pk=pk)
 	return render(request, 'velo/detailsplayer.html', {'players':players})
 
-def pdfdetailsplayer(request, pk):
-	# Model data
-	players = get_object_or_404(Player, pk=pk)
-
-	# Rendered
-	html_string = render_to_string('velo/detailsplayer.html', {'players': players})
-	html = HTML(string=html_string)
-	result = html.write_pdf()
-
-	# Creating http response
-	response = HttpResponse(content_type='application/pdf;')
-	response['Content-Disposition'] = 'inline; filename=preconisation_guidons.pdf'
-	response['Content-Transfer-Encoding'] = 'binary'
-
-	with tempfile.NamedTemporaryFile(delete=True) as output:
-		output.write(result)
-		output.flush()
-		output = open(output.name, 'r')
-		response.write(output.read())
-	return response
 
 def home(request):
 	return render(request, 'velo/home.html', {})
